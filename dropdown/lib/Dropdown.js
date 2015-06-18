bee51bd78586f3b17e2a7000cdf64c7949038380
João Valente <filipevalente@gmail.com>
2015-06-18 18:05:59 +0100

    adds dropdown component

---
+++ dropdown/lib/Dropdown.js
@@ -0,0 +1,108 @@
+(function() {
+  var Dropdown, OptionItem, Options, React, Trigger;
+
+  React = require('react');
+
+  Dropdown = React.createClass({
+    getInitialState: function() {
+      return {
+        show_options: false
+      };
+    },
+    _toggle: function() {
+      return this.setState({
+        show_options: !this.state.show_options
+      });
+    },
+    _renderMenuTrigger: function() {
+      var menu_trigger;
+      menu_trigger = null;
+      React.Children.forEach(this.props.children, (function(_this) {
+        return function(child) {
+          if (child.type === Trigger) {
+            return menu_trigger = React.cloneElement(child, {
+              onClick: _this._toggle
+            });
+          }
+        };
+      })(this));
+      return menu_trigger;
+    },
+    _renderMenuOptions: function() {
+      var menu_options;
+      if (!this.state.show_options) {
+        return false;
+      }
+      menu_options = null;
+      React.Children.forEach(this.props.children, (function(_this) {
+        return function(child) {
+          if (child.type === Options) {
+            return menu_options = React.createElement("div", {
+              "className": "o-dropdown__options_renderer"
+            }, React.cloneElement(child, {
+              hideOptions: _this._toggle,
+              on_top: _this.props.options_on_top
+            }));
+          }
+        };
+      })(this));
+      return menu_options;
+    },
+    render: function() {
+      return React.createElement("div", {
+        "className": "o-dropdown"
+      }, (this.props.options_on_top === true ? this._renderMenuOptions() : void 0), this._renderMenuTrigger(), (this.props.options_on_top !== true ? this._renderMenuOptions() : void 0));
+    }
+  });
+
+  Trigger = React.createClass({
+    render: function() {
+      console.log("will render trigger");
+      return React.createElement("div", {
+        "className": "o-dropdown__trigger",
+        "onClick": this.props.onClick
+      }, this.props.children);
+    }
+  });
+
+  Options = React.createClass({
+    _handleClick: function(event) {
+      if (this.getDOMNode().contains(event.target)) {
+
+      } else {
+        return this.props.hideOptions();
+      }
+    },
+    componentWillMount: function() {
+      return document.addEventListener("click", this._handleClick, false);
+    },
+    componentWillUnmount: function() {
+      return document.removeEventListener("click", this._handleClick, false);
+    },
+    render: function() {
+      var className;
+      className = "o-dropdown__options";
+      if (this.props.on_top) {
+        className += " o-dropdown__options--on_top";
+      }
+      return React.createElement("div", {
+        "className": className
+      }, React.createElement("ul", null, this.props.children));
+    }
+  });
+
+  OptionItem = React.createClass({
+    render: function() {
+      return React.createElement("li", null, this.props.children);
+    }
+  });
+
+  Dropdown.Trigger = Trigger;
+
+  Dropdown.Options = Options;
+
+  Dropdown.OptionItem = OptionItem;
+
+  module.exports = Dropdown;
+
+}).call(this);
---
+++ dropdown/src/Dropdown.cjsx
@@ -0,0 +1,80 @@
+React = require('react')
+
+Dropdown = React.createClass
+
+    getInitialState: ->
+        return { show_options: false }
+
+    _toggle: ->
+        @setState { show_options: !@state.show_options }
+
+    _renderMenuTrigger: ->
+        menu_trigger = null
+        React.Children.forEach(@props.children, (child) =>
+            if child.type == Trigger
+                menu_trigger = React.cloneElement(child, {onClick: @_toggle})
+        )
+        return menu_trigger
+
+    _renderMenuOptions: ->
+        if !@state.show_options
+            return false
+
+        menu_options = null
+        React.Children.forEach @props.children, (child) =>
+            if child.type == Options
+                menu_options =
+                    <div className = "o-dropdown__options_renderer">
+                        {React.cloneElement(child, {hideOptions: @_toggle, on_top: @props.options_on_top})}
+                    </div>
+
+        return menu_options
+
+    render: ->
+        <div className = "o-dropdown">
+            { @_renderMenuOptions() if @props.options_on_top == true }
+            { @_renderMenuTrigger() }
+            { @_renderMenuOptions() if @props.options_on_top != true }
+        </div>
+
+Trigger = React.createClass
+    render: ->
+        console.log "will render trigger"
+        <div className = "o-dropdown__trigger", onClick = @props.onClick>
+            { @props.children }
+        </div>
+
+Options = React.createClass
+    _handleClick: (event) ->
+        if @getDOMNode().contains(event.target)
+            return
+        else
+            @props.hideOptions()
+
+    # when options are displaying will be listening for clicks.
+    # will hide options if click outside.
+    componentWillMount: ->
+        document.addEventListener("click", @_handleClick, false)
+
+    componentWillUnmount: ->
+        document.removeEventListener("click", @_handleClick, false)
+
+    render: ->
+        className = "o-dropdown__options"
+        className += " o-dropdown__options--on_top" if @props.on_top
+        <div className = className>
+            <ul>
+                { @props.children }
+            </ul>
+        </div>
+
+OptionItem = React.createClass
+    render: ->
+        <li>
+            { @props.children }
+        </li>
+
+Dropdown.Trigger = Trigger
+Dropdown.Options = Options
+Dropdown.OptionItem = OptionItem
+module.exports = Dropdown
