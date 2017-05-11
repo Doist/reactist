aa8c50f1b407519786d74e71fbe931a5d1301c5e
Henning Muszynski <henningmu@users.noreply.github.com>
2017-05-11 08:14:34 +0200

    Implemented Time component incl. examples and docs

--- examples/time/src/TimeExamplePage.jsx
+++ examples/time/src/TimeExamplePage.jsx
@@ -1,13 +1,45 @@
 import React from 'react';
+import moment from 'moment';
 
 import { Time } from '@doist/reactist';
 
 class TimeExamplePage extends React.Component {
     render() {
+        const exampleTimes = [
+            { title: 'Now', time: moment().unix() },
+            { title: 'Now - 2 Minutes', time: moment().subtract(2, 'minutes').unix() },
+            { title: 'Now - 2 Hours', time: moment().subtract(2, 'hours').unix() },
+            { title: 'Now - 18 Hours', time: moment().subtract(18, 'hours').unix() },
+            { title: 'Now - 2 Days', time: moment().subtract(2, 'days').unix() },
+            { title: 'Now - 2 Weeks', time: moment().subtract(2, 'weeks').unix() },
+            { title: 'Now - 2 Years', time: moment().subtract(2, 'years').unix() }
+        ];
+
         return (
             <section>
                 <h1>Time Examples</h1>
-                <Time time={0} />
+                <h2>Normal Time (no hover effects)</h2>
+                {exampleTimes.map((time, index) => (
+                    <div key={index}>
+                        <span>{time.title}</span>
+                        <Time time={time.time} />
+                    </div>
+                ))}
+
+                <h2>Time expands on hover</h2>
+                {exampleTimes.map((time, index) => (
+                    <div key={index}>
+                        <span>{time.title}</span>
+                        <Time time={time.time} expandOnHover />
+                    </div>
+                ))}
+                <h2>Time expands fully on hover</h2>
+                {exampleTimes.map((time, index) => (
+                    <div key={index}>
+                        <span>{time.title}</span>
+                        <Time time={time.time} expandFullyOnHover />
+                    </div>
+                ))}
             </section>
         );
     }
---
+++ src/components/__tests__/Time.test.js
@@ -0,0 +1,59 @@
+import moment from 'moment';
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import Time from '../Time';
+
+describe('Time', () => {
+    const testDate = moment().year(1991)
+                             .month('March')
+                             .date(22)
+                             .hours(13)
+                             .minutes(37)
+                             .seconds(42)
+                             .unix();
+
+    it('renders without crashing', () => {
+        const time = shallow(<Time time={ moment().unix() } />);
+        expect(toJson(time)).toMatchSnapshot();
+    });
+
+    it('toggles hovered state on mouse enter and leave', () => {
+        const time = shallow(<Time time={ moment().unix() } />);
+        expect(time.state('hovered')).toBe(false);
+
+        time.simulate('mouseEnter');
+        expect(time.state('hovered')).toBe(true);
+
+        time.simulate('mouseLeave');
+        expect(time.state('hovered')).toBe(false);
+    });
+
+    it('renders relative time when not hovered', () => {
+        const time = shallow(<Time time={ moment().unix() } />);
+        expect(getRenderedTime(time)).toBe('moments ago');
+    });
+
+    it('renders full absolute time when hovered and expandedFullOnHover is set', () => {
+        const time = shallow(<Time time={ testDate } expandFullyOnHover />);
+        time.simulate('mouseEnter');
+        expect(getRenderedTime(time)).toBe('22 Mar 91, 1:37 PM');
+    });
+
+    it('renders short absolute time when hovered and expandedOnHover is set', () => {
+        const time = shallow(<Time time={ testDate } expandOnHover />);
+        time.simulate('mouseEnter');
+        expect(getRenderedTime(time)).toBe('Mar 22, 1991');
+    });
+
+    it('adds additional class name if supplied', () => {
+        const time = shallow(<Time time={ testDate } className="this-classes were-added" />);
+        expect(toJson(time)).toMatchSnapshot();
+    });
+
+    // Helper functions ///////////////////////////////////////////////////////
+    function getRenderedTime(timeComponent) {
+        return timeComponent.children().node;
+    }
+});
