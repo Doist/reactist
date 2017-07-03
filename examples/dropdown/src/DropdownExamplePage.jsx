f7aa71c434b60a5eb01c15c581fe8510d4227d3d
Henning Muszynski <henningmu@users.noreply.github.com>
2017-07-03 08:31:18 +0200

    Added example and documentation for allowBodyInteractions

--- examples/dropdown/src/DropdownExamplePage.jsx
+++ examples/dropdown/src/DropdownExamplePage.jsx
@@ -7,20 +7,39 @@ class DropdownExamplePage extends React.Component {
     _renderSimpleDropdown() {
         return (
             <Dropdown.Box>
-                    <Dropdown.Trigger>
-                        <div>Trigger: Click to show dropdown</div>
-                    </Dropdown.Trigger>
-                    <Dropdown.Body>
-                        <div>
-                            <h2>Dropdown Content</h2>
-                            <ul>
-                                <li>You can add anything</li>
-                                <li>you want to a dropdown</li>
-                                <li>(e.g. lists and headings)</li>
-                            </ul>
-                        </div>
-                    </Dropdown.Body>
-                </Dropdown.Box>
+                <Dropdown.Trigger>
+                    <div>Trigger: Click to show dropdown</div>
+                </Dropdown.Trigger>
+                <Dropdown.Body>
+                    <div>
+                        <h2>Dropdown Content</h2>
+                        <ul>
+                            <li>You can add anything</li>
+                            <li>you want to a dropdown</li>
+                            <li>(e.g. lists and headings)</li>
+                        </ul>
+                    </div>
+                </Dropdown.Body>
+            </Dropdown.Box>
+        )
+    }
+
+    _renderInteractiveBodyDropdown() {
+        return (
+            <Dropdown.Box allowBodyInteractions>
+                <Dropdown.Trigger>
+                    <div>Trigger: Click to show interactive dropdown (won't close when clicking on it)</div>
+                </Dropdown.Trigger>
+                <Dropdown.Body>
+                    <div>
+                        <h1>Wow this is interactive</h1>
+                        <ul>
+                            <li>Click outside</li>
+                            <li>to close this</li>
+                        </ul>
+                    </div>
+                </Dropdown.Body>
+            </Dropdown.Box>
         )
     }
 
@@ -30,6 +49,7 @@ class DropdownExamplePage extends React.Component {
                 <div id='modal_box'></div>
                 <h1>Dropdown Examples</h1>
                 { this._renderSimpleDropdown() }
+                { this._renderInteractiveBodyDropdown() }
             </section>
         )
     }
