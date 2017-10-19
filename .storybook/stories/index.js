14a32cff44009422af871fd63fce65f3dee9a46d
Henning Muszynski <henningmu@users.noreply.github.com>
2017-10-19 15:40:07 +0200

    Added story for time component and deleted example

--- .storybook/stories/index.js
+++ .storybook/stories/index.js
@@ -6,7 +6,9 @@ import { storiesOf } from '@storybook/react';
 import ReactistStory from './reactist/ReactistStory'
 import ButtonStory from './components/ButtonStory'
 import ModalStory from './components/ModalStory'
+import TimeStory from './components/TimeStory'
 
 ReactistStory()
 ButtonStory()
 ModalStory()
+TimeStory()
