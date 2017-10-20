4a3e62f1636d658f0cf8da3392652b617519615e
Henning Muszynski <henningmu@users.noreply.github.com>
2017-10-20 14:26:22 +0200

    Exposing ProgressBar correctly

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Modal from './components/Modal'
 import Button from './components/Button'
 import Time from './components/Time'
 import Dropdown from './components/Dropdown'
+import ProgressBar from './components/ProgressBar'
 
 export {
     Modal,
     Button,
     Time,
-    Dropdown
+    Dropdown,
+    ProgressBar
 }
