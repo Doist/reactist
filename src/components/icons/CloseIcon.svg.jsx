716f497fac7075de9992477e3a2138ef5e65c948
Henning Muszynski <henningmu@users.noreply.github.com>
2017-11-09 16:57:49 +0100

    Fixed the way svgs are handled in less and jsx

---
+++ src/components/icons/CloseIcon.svg.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CloseIcon = () => (
+    <svg style="fill:#B3B3B3" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12">
+        <path d="M12 1.427L10.573 0 5.997 4.576 1.424.003l-1.42 1.42 4.572 4.574L0 10.573 1.427 12l4.576-4.576 4.573 4.573 1.42-1.42-4.572-4.574L12 1.427z" fill-rule="evenodd">
+        </path>
+    </svg>
+)
+
+export default CloseIcon
