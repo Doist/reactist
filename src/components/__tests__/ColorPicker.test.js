5b329c29b09e3236cee7575701029f9793b1c1fa
Henning Muszynski <henningmu@users.noreply.github.com>
2017-11-10 13:22:12 +0100

    Added tests for ColorPicker

---
+++ src/components/__tests__/ColorPicker.test.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import ColorPicker, { ColorItem } from '../ColorPicker'
+
+describe('ColorPicker', () => {
+    it('renders without crashing', () => {
+        const colorPicker = shallow(getColorPicker())
+        expect(colorPicker).toMatchSnapshot()
+    })
+
+    it('renders all colors except the selected one', () => {
+        const colorPicker = shallow(getColorPicker({ color: 3 }))
+
+        const colorOptions = colorPicker.find('.color_options')
+        const selectedColorItem = colorOptions.find('ColorItem[colorIndex=3]')
+        expect(selectedColorItem.length).toBe(0)
+    })
+
+    describe('ColorItem', () => {
+        it('renders given color and does nothing when clicked without specified onClick handler', () => {
+            const colorItem = shallow(getColorItem())
+            colorItem.simulate('click')
+            expect(colorItem).toMatchSnapshot()
+        })
+
+        it('calls onClick after clicking it', () => {
+            const onClickSpy = jest.fn()
+            const colorItem = shallow(getColorItem({ colorIndex: 5, onClick: onClickSpy }))
+            colorItem.simulate('click')
+            expect(onClickSpy).toHaveBeenLastCalledWith(5)
+        })
+    })
+
+    // Helpers ================================================================
+    const getColorPicker = (props) => <ColorPicker { ...props } />
+    const getColorItem = (props) => <ColorItem colorIndex={ 0 } { ...props } />
+})
