eff932b4503e6318156e1d902ca4e82780976dfd
Henning Muszynski <henningmu@users.noreply.github.com>
2017-11-13 15:01:03 +0000

    Added new Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,30 @@
+import './styles/loading.less'
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+const Loading = ({ white, className }) => {
+    const loadingClass = classNames('spinner', {
+        'spinner--white': white
+    }, className)
+    return (
+        <div className={ loadingClass }>
+            <div className='spinner__dot' />
+            <div className='spinner__dot' />
+            <div className='spinner__dot' />
+        </div>
+    )
+}
+Loading.displayName = 'Loading'
+Loading.defaultProps = {
+    white: false
+}
+Loading.propTypes = {
+    /** Use white instead of default gray. */
+    white: PropTypes.bool,
+    /** Additional css class that is applied to Loading. */
+    className: PropTypes.string
+}
+
+export default Loading
