c8cfc9dd3a55461837a9767b3a2824aae416fab7
Henning Muszynski <henningmu@users.noreply.github.com>
2017-11-17 09:16:22 +0000

    Replaced stroke-width property with strokeWidth

--- src/components/icons/ThinQuestionMarkIcon.svg.jsx
+++ src/components/icons/ThinQuestionMarkIcon.svg.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const ThinQuestionMark = () => (
-    <svg width="18px" height="18px" viewBox="0 0 18 18">
-        <g stroke="none" stroke-width="1" fill="none" fillRule="evenodd">
+    <svg width='18px' height='18px' viewBox='0 0 18 18'>
+        <g stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
             <g fill='#979797'>
                 <path
                     d='M9,17 C13.418278,17 17,13.418278 17,9 C17,4.581722 13.418278,1 9,1 C4.581722,1 1,4.581722 1,9 C1,13.418278 4.581722,17 9,17 Z M9,18 C4.02943725,18 0,13.9705627 0,9 C0,4.02943725 4.02943725,0 9,0 C13.9705627,0 18,4.02943725 18,9 C18,13.9705627 13.9705627,18 9,18 Z'
