49eab6ea556f387901506f3b2412537d2a413609
Henning Muszynski <henningmu@users.noreply.github.com>
2017-11-29 14:50:38 +0100

    Only toggle hovered state when mouse moved to avoid flickering

--- src/components/__tests__/Time.test.js
+++ src/components/__tests__/Time.test.js
@@ -19,15 +19,26 @@ describe('Time', () => {
         expect(toJson(time)).toMatchSnapshot()
     })
 
-    it('toggles hovered state on mouse enter and leave', () => {
+    it('toggles hovered state on mouse enter and leave when mouse moves', () => {
         const time = shallow(<Time time={ moment().unix() } />)
         expect(time.state('hovered')).toBe(false)
 
-        time.simulate('mouseEnter')
+        time.simulate('mouseEnter', getMouseEvent(100, 100))
         expect(time.state('hovered')).toBe(true)
 
-        time.simulate('mouseLeave')
+        time.simulate('mouseLeave', getMouseEvent(100, 110))
         expect(time.state('hovered')).toBe(false)
+    })
+
+    it('does not toggle hovered state when mouse did not move', () => {
+        const time = shallow(<Time time={ moment().unix() } />)
+        expect(time.state('hovered')).toBe(false)
+
+        time.simulate('mouseEnter', getMouseEvent())
+        expect(time.state('hovered')).toBe(true)
+
+        time.simulate('mouseLeave', getMouseEvent())
+        expect(time.state('hovered')).toBe(true)
     })
 
     it('renders relative time when not hovered', () => {
@@ -37,13 +48,13 @@ describe('Time', () => {
 
     it('renders full absolute time when hovered and expandedFullOnHover is set', () => {
         const time = shallow(<Time time={ testDate } expandFullyOnHover />)
-        time.simulate('mouseEnter')
+        time.simulate('mouseEnter', getMouseEvent())
         expect(getRenderedTime(time)).toBe('22 Mar 91, 1:37 PM')
     })
 
     it('renders short absolute time when hovered and expandedOnHover is set', () => {
         const time = shallow(<Time time={ testDate } expandOnHover />)
-        time.simulate('mouseEnter')
+        time.simulate('mouseEnter', getMouseEvent())
         expect(getRenderedTime(time)).toBe('Mar 22, 1991')
     })
 
@@ -55,5 +66,9 @@ describe('Time', () => {
     // Helper functions ///////////////////////////////////////////////////////
     function getRenderedTime(timeComponent) {
         return timeComponent.children().node
+    }
+
+    function getMouseEvent(x = 100, y = 100) {
+        return { clientX: x, clientY: y }
     }
 })
