9ed0d6a76ad03b267281840746dc0de36197d35c
Henning Muszynski <henningmu@users.noreply.github.com>
2018-06-07 10:59:50 +0200

    Updated tests and story for new ColorPicker feature

--- .storybook/stories/components/ColorPickerStory.js
+++ .storybook/stories/components/ColorPickerStory.js
@@ -45,10 +45,43 @@ const ColorPickersChapter = {
     ]
 }
 
+class CustomColorPickersStory extends React.Component {
+    constructor(props, context) {
+        super(props, context)
+        this.state = { color: 0 }
+    }
+
+    render() {
+        return (
+            <section className="story">
+                <ColorPicker
+                    color={this.state.color}
+                    onChange={color => this.setState(() => ({ color }))}
+                    colorList={['red', 'green', 'palegoldenrod', '#FF00FF']}
+                />
+            </section>
+        )
+    }
+}
+const CustomColorPickersChapter = {
+    subtitle: 'Custom Colors Color Picker',
+    sections: [
+        {
+            // eslint-disable-next-line react/display-name
+            sectionFn: () => <CustomColorPickersStory />,
+            options: optionsNoSourceNoProps
+        }
+    ]
+}
+
 // Story setup ================================================================
 const Story = () =>
     storiesOf('ColorPicker', module).addWithChapters('Component Overview', {
-        chapters: [ColorPickerPropTypesChapter, ColorPickersChapter]
+        chapters: [
+            ColorPickerPropTypesChapter,
+            ColorPickersChapter,
+            CustomColorPickersChapter
+        ]
     })
 
 export default Story
