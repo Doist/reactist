7e367e4bdbe2790c0ccb34b2d5d6f0e257f8595d
Kevin Ndung'u <kevgathuku@gmail.com>
2018-08-21 13:27:19 +0300

    Fix typo on ReactistStory

--- .storybook/stories/reactist/ReactistStory.md.js
+++ .storybook/stories/reactist/ReactistStory.md.js
@@ -9,7 +9,7 @@ const howToText = `
     \`\`\`
     If you prefer to include static files just grab the [minified build from the dist folder](https://github.com/Doist/reactist/tree/develop/dist).
 
-    A detailled explanation and exmaples on how to use each component can be accessed by clicking on the component name on the left.
+    A detailled explanation and examples on how to use each component can be accessed by clicking on the component name on the left.
 `
 
 const developmentText = `
