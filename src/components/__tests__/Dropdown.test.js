256032f1fecf2e1d908f23b6c03557f101b9e75b
Jacob Sowles <jrsowles@gmail.com>
2018-12-10 20:59:05 -0800

    Added Dropdown.Body tests

--- src/components/__tests__/Dropdown.test.js
+++ src/components/__tests__/Dropdown.test.js
@@ -4,6 +4,40 @@ import toJson from 'enzyme-to-json'
 
 import Dropdown from '../Dropdown'
 
+describe('Dropdown.Body', () => {
+    it('renders empty without crashing', () => {
+        const body = shallow(<Dropdown.Body />)
+        expect(toJson(body)).toMatchSnapshot()
+    })
+
+    it('renders all children without crashing', () => {
+        const simpleBody = shallow(<Dropdown.Body>Body Content</Dropdown.Body>)
+        expect(toJson(simpleBody)).toMatchSnapshot()
+
+        const complexBody = shallow(
+            <Dropdown.Body>
+                <div>
+                    <img src="" alt="" />
+                    Some additional text as well
+                </div>
+            </Dropdown.Body>
+        )
+        expect(toJson(complexBody)).toMatchSnapshot()
+    })
+
+    it('opens to the top when top is supplied', () => {
+        const body = shallow(<Dropdown.Body top />)
+        expect(body.props().style.top).toBe('auto')
+        expect(body.props().style.bottom).toBe(0)
+    })
+
+    it('opens to the right when top is supplied', () => {
+        const body = shallow(<Dropdown.Body right />)
+        expect(body.props().style.right).toBe('auto')
+        expect(body.props().style.left).toBe(0)
+    })
+})
+
 describe('Dropdown.Trigger', () => {
     it('renders empty without crashing', () => {
         const trigger = shallow(<Dropdown.Trigger />)
