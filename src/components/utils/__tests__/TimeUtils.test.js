5bbfc922d0eb1e52ef9e3fc7c64055881da01099
gnarmedia <gnarmedia@users.noreply.github.com>
2019-03-12 01:22:29 -0700

    Replace moment with dayjs in TimeUtils

--- src/components/utils/__tests__/TimeUtils.test.js
+++ src/components/utils/__tests__/TimeUtils.test.js
@@ -1,91 +1,66 @@
-import moment from 'moment'
+import dayjs from 'dayjs'
+import MockDate from 'mockdate'
 
 import TimeUtils from '../TimeUtils'
 
-// setup mocked date generation
-let now = null
 beforeAll(() => {
-    // whenever moment() is invoked 2017-03-22 will be taken as date
-    Date.now = jest.fn(() => new Date(Date.UTC(2017, 2, 23, 0, 0, 0)).valueOf())
-    now = moment()
+    // whenever dayjs() is invoked 2017-03-22 will be taken as date
+    MockDate.set(new Date(Date.UTC(2017, 2, 23)))
 })
 
 describe('Time Utils', () => {
-    it('formatTime shoud match `LL` when in current year', () => {
-        const testDate = moment()
-            .year(now.year())
-            .month('March')
-            .date(9)
-            .unix()
+    it('formatTime should match `LL` when in current year', () => {
+        const testDate = dayjs(new Date(dayjs().year(), 2, 9)).unix()
         expect(TimeUtils.formatTime(testDate)).toBe('03/09/2017')
     })
 
     it('formatTime should match `LL` when in past year', () => {
-        const testDate = moment()
-            .year(2016)
-            .month('March')
-            .date(9)
-            .unix()
+        const testDate = dayjs(new Date('March 9, 2016')).unix()
         expect(TimeUtils.formatTime(testDate)).toBe('March 9, 2016')
     })
 
     it('formatTimeLong should match `LL, LT`', () => {
-        const testDate = moment()
-            .year(2017)
-            .month('August')
-            .date(1)
-            .hours(13)
-            .minutes(37)
-            .seconds(42)
-            .unix()
+        const testDate = dayjs(new Date('August 1, 2017 13:37:42')).unix()
         expect(TimeUtils.formatTimeLong(testDate)).toBe(
             'August 1, 2017, 1:37 PM'
         )
     })
 
     it('timeAgo < 1m returns `moments ago`', () => {
-        const testDate = moment()
+        const testDate = dayjs()
             .subtract(10, 'seconds')
             .unix()
         expect(TimeUtils.timeAgo(testDate)).toBe('moments ago')
     })
 
     it('timeAgo < 1h returns `XXm`', () => {
-        const testDate = moment()
+        const testDate = dayjs()
             .subtract(10, 'minutes')
             .unix()
         expect(TimeUtils.timeAgo(testDate)).toBe('10m')
     })
 
     it('timeAgo < 1d returns `XXh`', () => {
-        const testDate = moment()
+        const testDate = dayjs()
             .subtract(10, 'hours')
             .unix()
         expect(TimeUtils.timeAgo(testDate)).toBe('10h')
     })
 
     it('timeAgo < 7d returns MM/DD/YYYY', () => {
-        const testDate = moment()
+        const testDate = dayjs()
             .subtract(2, 'days')
             .unix()
         expect(TimeUtils.timeAgo(testDate)).toBe('03/20/2017')
     })
 
     it('timeAgo > 7d returns `MM/DD/YYYY` in current year', () => {
-        const testDate = moment()
-            .year(now.year())
-            .month('March')
-            .date(9)
-            .unix()
+        const testDate = dayjs(new Date(dayjs().year(), 2, 9)).unix()
         expect(TimeUtils.timeAgo(testDate)).toBe('03/09/2017')
     })
 
     it('timeAgo > 7d returns `LL` in past year', () => {
-        const testDate = moment()
-            .year(2016)
-            .month('March')
-            .date(9)
-            .unix()
+        const testDate = dayjs(new Date('March 9, 2016')).unix()
         expect(TimeUtils.timeAgo(testDate)).toBe('March 9, 2016')
     })
 })
