c4f5369f8239eb766274b6fa28d80184994f082c
Henning Muszynski <henningmu@users.noreply.github.com>
2019-07-18 10:57:10 +0200

    Updated static build artifacts

--- docs/main.26063916f3d9cfc43efb.bundle.js
+++ docs/main.26063916f3d9cfc43efb.bundle.js
@@ -1,14 +1,1 @@
-;(window.webpackJsonp = window.webpackJsonp || []).push([
-    [0],
-    {
-        375: function(n, o, p) {
-            p(376), p(457), (n.exports = p(786))
-        },
-        457: function(n, o, p) {
-            'use strict'
-            p.r(o)
-            p(458), p(474), p(743)
-        }
-    },
-    [[375, 1, 2]]
-])
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{375:function(n,o,p){p(376),p(457),n.exports=p(786)},457:function(n,o,p){"use strict";p.r(o);p(458),p(474),p(743)}},[[375,1,2]]]);
