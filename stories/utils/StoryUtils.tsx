f913f8dabcaeef5ac31505527cc6502247583333
Vishnu Gopal <vishnugopal@users.noreply.github.com>
2020-05-19 18:05:01 +0530

    Fix or defer lots of linter issues

--- stories/utils/StoryUtils.tsx
+++ stories/utils/StoryUtils.tsx
@@ -24,7 +24,7 @@ const optionsNoSourceNoProps = {
     showPropTables: false,
     allowPropTablesToggling: false,
 }
-const emptySection = () => undefined
+const emptySection = () => {}
 
 export {
     getPropTypesStory,
