d1b233c7881e7998e042bf0ca2995c77b8e18277
Vishnu Gopal <vishnugopal@users.noreply.github.com>
2020-05-19 17:27:26 +0530

    Delete some unneeded mocks

--- lib/__mocks__/fileMock.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: "test-file-stub";
-/**
- * This mock is used by jest to be able to load files.
- * It returns the same string for every file loaded during the tests.
- */
-export default _default;
