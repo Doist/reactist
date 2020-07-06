cb8cabd101655c276f895a57d025f781e64d7a66
Vishnu Gopal <vishnugopal@users.noreply.github.com>
2020-07-06 14:08:34 +0530

    Add rollup configuration, storybook configuration et. al. for CSS modules with inline SVG styles to work

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,39 +1,24 @@
-const path = require('path')
-const postcss = require('rollup-plugin-postcss')
-const autoprefixer = require('autoprefixer')
-const cssnano = require('cssnano')
-const url = require('postcss-url')
-const getComponentsMap = require('./scripts/buildHelpers').getComponentsMap
+const styles = require('rollup-plugin-styles')
 
 module.exports = {
     rollup(config, options) {
         if (config.output.format === 'esm') {
-            config.input = {
-                ...getComponentsMap(path.resolve(__dirname, './src/components')),
-                index: path.resolve(__dirname, './src/index.ts'),
-            }
+            config.output.dir = 'es'
+        } else {
             config.output.dir = 'dist'
-            config.output.file = undefined
-            config.output.entryFileNames = '[name]/index.js'
-            config.output.hoistTransitiveImports = false
         }
-        config.plugins.push(
-            postcss({
-                plugins: [
-                    autoprefixer(),
-                    cssnano({
-                        preset: 'default',
-                    }),
-                    url({
-                        url: 'inline',
-                        maxSize: 10,
-                        fallback: 'copy',
-                    }),
-                ],
-                inject: false,
-                extract: 'reactist.css',
+        config.preserveModules = true
+        config.output.file = undefined
+        config.output.assetFileNames = '[name][extname]'
+        config.output.chunkFileNames = '[name]'
+        config.plugins = [
+            styles({
+                autoModules: /index/,
+                mode: 'extract',
+                url: { inline: true },
             }),
-        )
+        ].concat(config.plugins)
+
         return config
     },
 }
