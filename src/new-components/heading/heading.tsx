67eadbd194de22f43aebeca99c01f5a635e25157
Frankie Yan <frankieyan@gmail.com>
2021-03-05 16:22:39 +0800

    Fix inherited Heading element props

--- src/new-components/heading/heading.tsx
+++ src/new-components/heading/heading.tsx
@@ -5,14 +5,21 @@ import { Box } from '../box'
 import styles from './heading.module.css'
 
 type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6 | '1' | '2' | '3' | '4' | '5' | '6'
+type HeadingElement = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 
-interface HeadingProps extends Omit<HTMLHeadingElement, 'className'> {
+interface HeadingProps
+    extends Omit<JSX.IntrinsicElements[HeadingElement], 'className' | 'children'> {
     level: HeadingLevel
     weight?: 'regular' | 'light'
     size?: 'smaller' | 'larger'
+    children: React.ReactNode
 }
 
 function Heading({ level, weight = 'regular', size, children, ...props }: HeadingProps) {
+    // In TypeScript v4.1, this would be properly recognized without needing the type assertion
+    // https://devblogs.microsoft.com/typescript/announcing-typescript-4-1-beta/#template-literal-types
+    const headingElementName = `h${level}` as HeadingElement
+
     return (
         <Box
             className={[
@@ -20,7 +27,7 @@ function Heading({ level, weight = 'regular', size, children, ...props }: Headin
                 weight !== 'regular' ? getClassNames(styles, 'weight', weight) : null,
                 getClassNames(styles, 'size', size),
             ]}
-            component={`h${level}` as 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'}
+            component={headingElementName}
             {...props}
         >
             {children}
