2ab3bd5cb3f46595b8b3729d0713ba3413f47479
Frankie Yan <frankieyan@gmail.com>
2021-04-30 18:34:34 +0800

    fix: Pass component prop through in Column

--- src/new-components/columns/columns.tsx
+++ src/new-components/columns/columns.tsx
@@ -42,6 +42,7 @@ const Column = forwardRefWithAs<ColumnProps>(function Column(
             width={width !== 'content' ? 'full' : undefined}
             flexShrink={width === 'content' ? 0 : undefined}
             ref={ref}
+            component={component}
             {...props}
         >
             {children}
