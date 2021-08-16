eb6da74ae88a9f23e2be7b0ba342afb5717c7cc5
Ernesto Garcia <gnapse@gmail.com>
2021-08-16 16:13:52 -0400

    Add a storybook example about Box padding

--- src/new-components/box/box.stories.tsx
+++ src/new-components/box/box.stories.tsx
@@ -1,41 +1,34 @@
 import * as React from 'react'
+
 import {
     select,
     selectWithNone,
     reusableBoxProps,
-    disableResponsiveProps,
     Placeholder,
     ResponsiveWidthRef,
     Wrapper,
     PartialProps,
+    selectSize,
 } from '../storybook-helper'
+
+import { Box } from './box'
+import { Inline } from '../inline'
 import { Stack } from '../stack'
-import { Box } from './'
+import { Text } from '../text'
+
 import type {
     BoxDisplay,
     BoxFlexDirection,
     BoxFlexWrap,
     BoxAlignItems,
     BoxJustifyContent,
+    BoxPaddingProps,
 } from './'
+import type { Space } from '../common-types'
 
 export default {
     title: 'Design system/Box',
     component: Box,
-    argTypes: {
-        display: select<BoxDisplay>(['block', 'inlineBlock', 'inline', 'flex', 'none'], 'block'),
-        flexDirection: selectWithNone<BoxFlexDirection>(['column', 'row'], 'row'),
-        flexWrap: selectWithNone<BoxFlexWrap>(['wrap', 'nowrap'], 'nowrap'),
-        alignItems: selectWithNone<BoxAlignItems>(
-            ['center', 'flexEnd', 'flexStart', 'baseline'],
-            'none',
-        ),
-        justifyContent: selectWithNone<BoxJustifyContent>(
-            ['center', 'flexEnd', 'flexStart', 'spaceBetween'],
-            'none',
-        ),
-        ...reusableBoxProps(),
-    },
 }
 
 export function InteractivePropsStory(args: PartialProps<typeof Box>) {
@@ -48,6 +41,21 @@ export function InteractivePropsStory(args: PartialProps<typeof Box>) {
             </Box>
         </Wrapper>
     )
+}
+
+InteractivePropsStory.argTypes = {
+    display: select<BoxDisplay>(['block', 'inlineBlock', 'inline', 'flex', 'none'], 'block'),
+    flexDirection: selectWithNone<BoxFlexDirection>(['column', 'row'], 'row'),
+    flexWrap: selectWithNone<BoxFlexWrap>(['wrap', 'nowrap'], 'nowrap'),
+    alignItems: selectWithNone<BoxAlignItems>(
+        ['center', 'flexEnd', 'flexStart', 'baseline'],
+        'none',
+    ),
+    justifyContent: selectWithNone<BoxJustifyContent>(
+        ['center', 'flexEnd', 'flexStart', 'spaceBetween'],
+        'none',
+    ),
+    ...reusableBoxProps(),
 }
 
 export function ResponsiveStory() {
@@ -89,11 +97,35 @@ export function ResponsiveStory() {
     )
 }
 
-ResponsiveStory.argTypes = {
-    display: { control: false },
-    flexDirection: { control: false },
-    flexWrap: { control: false },
-    alignItems: { control: false },
-    justifyContent: { control: false },
-    ...disableResponsiveProps,
+function PaddedBox({ prop, value }: { prop: keyof BoxPaddingProps; value: Space }) {
+    const paddingProp = { [prop]: value }
+    return (
+        <Box borderRadius="standard" style={{ backgroundColor: '#83A9F2' }} {...paddingProp}>
+            <Box borderRadius="standard" padding="medium" style={{ backgroundColor: '#1D438C' }}>
+                <Text style={{ color: 'white', fontFamily: 'sans-serif' }}>{prop}</Text>
+            </Box>
+        </Box>
+    )
+}
+
+export function PaddingStory({ padding }: { padding: Space }) {
+    return (
+        <Stack space="large">
+            <Inline space="large" align="center" alignY="center">
+                <PaddedBox prop="padding" value={padding} />
+                <PaddedBox prop="paddingX" value={padding} />
+                <PaddedBox prop="paddingY" value={padding} />
+            </Inline>
+            <Inline space="large" align="center" alignY="center">
+                <PaddedBox prop="paddingTop" value={padding} />
+                <PaddedBox prop="paddingRight" value={padding} />
+                <PaddedBox prop="paddingBottom" value={padding} />
+                <PaddedBox prop="paddingLeft" value={padding} />
+            </Inline>
+        </Stack>
+    )
+}
+
+PaddingStory.argTypes = {
+    padding: selectSize('medium'),
 }
