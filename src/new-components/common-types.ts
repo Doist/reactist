77221d7ec8acb3cb9c14210579bf541b69a5035e
Ernesto García <ernesto@doist.com>
2021-08-17 17:10:10 -0400

    Box component improvements (#536)
    * Extract Box's padding styles into a separate css module
    * Add a storybook example about Box padding
    * Add support to apply margin props to Box
    * Add new Box maxWidth value "full"

--- src/new-components/common-types.ts
+++ src/new-components/common-types.ts
@@ -2,6 +2,18 @@ import classNames from 'classnames'
 import type { HTMLAttributes } from 'react'
 
 export type Space = 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge'
+
+export type SpaceWithNegatives =
+    | Space
+    // once we update to TS 4.1 or higher, we can replace the list below with `-${Space}`
+    // see https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html
+    | '-xsmall'
+    | '-small'
+    | '-medium'
+    | '-large'
+    | '-xlarge'
+    | '-xxlarge'
+
 export type Tone = 'normal' | 'secondary' | 'danger'
 export type AlertTone = 'info' | 'positive' | 'caution' | 'critical'
 
