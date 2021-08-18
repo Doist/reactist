4b764f7051427ea3b99309de298f2426ac8fce30
Ernesto Garcia <gnapse@gmail.com>
2021-08-18 17:39:16 -0400

    Add tests, stories and improvements to SwitchField

--- src/new-components/switch-field/switch-field.tsx
+++ src/new-components/switch-field/switch-field.tsx
@@ -1,46 +1,80 @@
 import * as React from 'react'
+import { VisuallyHidden } from 'reakit/VisuallyHidden'
 import { Box } from '../box'
 import { FieldComponentProps, FieldHint } from '../base-field'
 import styles from './switch-field.module.css'
 import { useId } from '../common-helpers'
+import { Stack } from '../stack'
 
-function join(...args: Array<string | null | undefined>) {
-    const list = args.map((s) => s?.trim()).filter(Boolean)
-    return list.length > 0 ? list.join(' ') : undefined
-}
+type SwitchFieldProps = Omit<
+    FieldComponentProps<HTMLInputElement>,
+    'type' | 'secondaryLabel' | 'auxiliaryLabel' | 'maxWidth'
+>
 
-type SwitchFieldProps = FieldComponentProps<HTMLInputElement>
-
-function SwitchField({
-    label,
-    hint,
-    disabled = false,
-    checked = false,
-    maxWidth,
-    ...props
-}: SwitchFieldProps) {
-    const id = useId(props.id)
+const SwitchField = React.forwardRef<HTMLInputElement, SwitchFieldProps>(function SwitchField(
+    {
+        label,
+        hint,
+        disabled = false,
+        hidden,
+        defaultChecked,
+        id: originalId,
+        'aria-describedby': originalAriaDescribedBy,
+        onChange,
+        ...props
+    },
+    ref,
+) {
+    const id = useId(originalId)
     const hintId = useId()
+    const ariaDescribedBy = originalAriaDescribedBy ?? (hint ? hintId : undefined)
+    const [checkedState, setChecked] = React.useState(props.checked ?? defaultChecked ?? false)
+    const isChecked = props.checked ?? checkedState
     return (
-        <Box className={[styles.container, disabled ? styles.disabled : null]} maxWidth={maxWidth}>
-            <Box as="label" display="flex" alignItems="center">
-                <Box className={[styles.inputWrapper, checked ? styles.checked : null]}>
-                    <input
-                        id={id}
-                        type="checkbox"
-                        checked={checked}
-                        disabled={disabled}
-                        aria-describedby={join(hint ? hintId : null, props['aria-describedby'])}
-                        {...props}
-                    />
+        <Stack space="small" hidden={hidden}>
+            <Box
+                className={[
+                    styles.container,
+                    disabled ? styles.disabled : null,
+                    isChecked ? styles.checked : null,
+                ]}
+                as="label"
+                display="flex"
+                alignItems="center"
+            >
+                <Box
+                    position="relative"
+                    display="inlineBlock"
+                    overflow="visible"
+                    marginRight="small"
+                    flexShrink={0}
+                    className={styles.toggle}
+                >
+                    <VisuallyHidden>
+                        <input
+                            {...props}
+                            id={id}
+                            type="checkbox"
+                            disabled={disabled}
+                            aria-describedby={ariaDescribedBy}
+                            ref={ref}
+                            checked={isChecked}
+                            onChange={(event) => {
+                                onChange?.(event)
+                                if (!event.defaultPrevented) {
+                                    setChecked(event.currentTarget.checked)
+                                }
+                            }}
+                        />
+                    </VisuallyHidden>
                     <span className={styles.handle} />
                 </Box>
-                <span>{label}</span>
+                <span className={styles.label}>{label}</span>
             </Box>
             {hint ? <FieldHint id={hintId}>{hint}</FieldHint> : null}
-        </Box>
+        </Stack>
     )
-}
+})
 
 export { SwitchField }
 export type { SwitchFieldProps }
