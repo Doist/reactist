de61a7c1e2329eb3e5acd87c98415348bc5b7179
Frankie Yan <frankieyan@users.noreply.github.com>
2021-09-02 00:55:19 +0800

    feat: Support polymorphism with `TabPanel` component (#551)
    * feat: Make TabPanel polymorphic
    * Switch to inline exports so docgen is able to grab type info
    * Add test for className prop on TabPanel
    * Update changelog
    
    Co-authored-by: Ernesto Garcia <gnapse@gmail.com>

--- src/new-components/tabs/tabs.test.tsx
+++ src/new-components/tabs/tabs.test.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { screen, render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+
+import { LoadingSpinner } from '../loading-spinner'
 import { Tabs, Tab, TabList, TabPanel, TabAwareSlot } from './'
 
 describe('Tabs', () => {
@@ -190,5 +192,53 @@ describe('Tabs', () => {
 
         userEvent.click(screen.getByRole('tab', { name: 'Tab 3' }))
         expect(screen.getByText('Currently rendering tab3')).toBeVisible()
+    })
+
+    it('allows different elements to be rendered in place of the TabPanel', () => {
+        render(
+            <Tabs>
+                <TabList aria-label="Multiple tablist example tabs">
+                    <Tab id="tab1">Tab 1</Tab>
+                    <Tab id="tab2">Tab 2</Tab>
+                </TabList>
+                <TabPanel id="tab1" as={LoadingSpinner} label="Loading">
+                    It makes no sense to render a loading spinner here, but it's an easier component
+                    to query for to make sure we're rendering our custom element
+                </TabPanel>
+                <TabPanel id="tab2" as="section">
+                    Content of tab 2
+                </TabPanel>
+            </Tabs>,
+        )
+
+        // The LoadingSpinner doesn't allow its role to be overridden nor render children
+        expect(screen.getByRole('alert', { name: 'Loading' })).toBeVisible()
+        expect(screen.queryByText(/It makes no sense/)).not.toBeInTheDocument()
+
+        userEvent.click(screen.getByRole('tab', { name: 'Tab 2' }))
+        expect(screen.getByRole('tabpanel', { name: 'Tab 2' })).toBeVisible()
+        expect(screen.getByRole('tabpanel', { name: 'Tab 2' })).toBe(
+            document.querySelector('section'),
+        )
+        expect(screen.getByText('Content of tab 2')).toBeVisible()
+    })
+
+    // eslint-disable-next-line jest/expect-expect
+    it('disallows className prop on TabPanel', () => {
+        render(
+            <Tabs>
+                <TabList aria-label="test-tabs">
+                    <Tab id="tab1">Tab 1</Tab>
+                    <Tab id="tab2">Tab 2</Tab>
+                    <Tab id="tab3">Tab 3</Tab>
+                </TabList>
+                {/* @ts-expect-error */}
+                <TabPanel id="tab1" className="foo">
+                    Content of tab 1
+                </TabPanel>
+                <TabPanel id="tab2">Content of tab 2</TabPanel>
+                <TabPanel id="tab3">Content of tab 3</TabPanel>
+            </Tabs>,
+        )
     })
 })
--- src/new-components/tabs/tabs.tsx
+++ src/new-components/tabs/tabs.tsx
@@ -9,6 +9,7 @@ import {
 } from 'reakit/Tab'
 import { Inline } from '../inline'
 import { usePrevious } from '../../hooks/use-previous'
+import { polymorphicComponent } from '../../utils/polymorphism'
 import type { ResponsiveProp } from '../responsive-props'
 import type { Space } from '../common-types'
 
@@ -38,7 +39,7 @@ type TabsProps = {
 /**
  * Used to group components that compose a set of tabs. There can only be one active tab within the same `<Tabs>` group.
  */
-function Tabs({
+export function Tabs({
     children,
     selectedId,
     color = 'primary',
@@ -89,7 +90,7 @@ type TabProps = {
 /**
  * Represents the individual tab elements within the group. Each `<Tab>` must have a corresponding `<TabPanel>` component.
  */
-function Tab({ children, id }: TabProps): React.ReactElement | null {
+export function Tab({ children, id }: TabProps): React.ReactElement | null {
     const tabContextValue = React.useContext(TabsContext)
 
     if (!tabContextValue) {
@@ -146,7 +147,7 @@ type TabListProps = (
 /**
  * A component used to group `<Tab>` elements together.
  */
-function TabList({
+export function TabList({
     children,
     space = 'medium',
     ...props
@@ -184,34 +185,39 @@ type TabPanelProps = {
 /**
  * Used to define the content to be rendered when a tab is active. Each `<TabPanel>` must have a corresponding `<Tab>` component.
  */
-function TabPanel({ children, id, render = 'always' }: TabPanelProps): React.ReactElement | null {
-    const tabContextValue = React.useContext(TabsContext)
-    const [tabRendered, setTabRendered] = React.useState(false)
-    const tabIsActive = tabContextValue?.selectedId === id
+export const TabPanel = polymorphicComponent<'div', TabPanelProps, 'omitClassName'>(
+    function TabPanel(
+        { children, id, as, render = 'always', ...props },
+        ref,
+    ): React.ReactElement | null {
+        const tabContextValue = React.useContext(TabsContext)
+        const [tabRendered, setTabRendered] = React.useState(false)
+        const tabIsActive = tabContextValue?.selectedId === id
 
-    React.useEffect(
-        function trackTabRenderedState() {
-            if (!tabRendered && tabIsActive) {
-                setTabRendered(true)
-            }
-        },
-        [tabRendered, tabIsActive],
-    )
+        React.useEffect(
+            function trackTabRenderedState() {
+                if (!tabRendered && tabIsActive) {
+                    setTabRendered(true)
+                }
+            },
+            [tabRendered, tabIsActive],
+        )
 
-    if (!tabContextValue) {
-        return null
-    }
+        if (!tabContextValue) {
+            return null
+        }
 
-    const { color, variant, ...tabState } = tabContextValue
+        const { color, variant, ...tabState } = tabContextValue
 
-    return (
-        <BaseTabPanel tabId={id} {...tabState}>
-            {render === 'always' ? children : null}
-            {render === 'active' && tabIsActive ? children : null}
-            {render === 'lazy' && (tabIsActive || tabRendered) ? children : null}
-        </BaseTabPanel>
-    )
-}
+        return (
+            <BaseTabPanel tabId={id} {...tabState} {...props} as={as} ref={ref}>
+                {render === 'always' ? children : null}
+                {render === 'active' && tabIsActive ? children : null}
+                {render === 'lazy' && (tabIsActive || tabRendered) ? children : null}
+            </BaseTabPanel>
+        )
+    },
+)
 
 type TabAwareSlotProps = {
     /**
@@ -225,10 +231,8 @@ type TabAwareSlotProps = {
  * Allows content to be rendered based on the current tab being selected while outside of the TabPanel
  * component. Can be placed freely within the main `<Tabs>` component.
  */
-function TabAwareSlot({ children }: TabAwareSlotProps): React.ReactElement | null {
+export function TabAwareSlot({ children }: TabAwareSlotProps): React.ReactElement | null {
     const tabContextValue = React.useContext(TabsContext)
 
     return tabContextValue ? children({ selectedId: tabContextValue.selectedId }) : null
 }
-
-export { Tabs, TabList, Tab, TabPanel, TabAwareSlot }
