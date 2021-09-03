1fcd1c1107ff87f55af7989094623eb6a42b4653
Ernesto Garcia <gnapse@gmail.com>
2021-09-03 00:37:45 -0400

    Make the new loading component more flexible regarding label and style

--- src/new-components/loading/loading.test.tsx
+++ src/new-components/loading/loading.test.tsx
@@ -12,26 +12,40 @@ function getSize() {
 }
 
 describe('Loading', () => {
+    it('does not acknowledge the className prop, but exceptionallySetClassName instead', () => {
+        render(
+            <Loading
+                aria-label="Loading…"
+                // @ts-expect-error
+                className="wrong"
+                exceptionallySetClassName="right"
+            />,
+        )
+        const loadingIndicator = screen.getByRole('progressbar', { name: 'Loading…' })
+        expect(loadingIndicator).toHaveClass('right')
+        expect(loadingIndicator).not.toHaveClass('wrong')
+    })
+
     it('renders an indeterminate progressbar', () => {
-        render(<Loading label="Loading…" />)
+        render(<Loading aria-label="Loading…" />)
         expect(screen.getByRole('progressbar', { name: 'Loading…' })).toBeInTheDocument()
     })
 
     it('renders in small size by default', () => {
-        const { rerender } = render(<Loading label="Loading…" />)
+        const { rerender } = render(<Loading aria-label="Loading…" />)
         const originalSize = getSize()
-        rerender(<Loading label="Loading…" size="small" />)
+        rerender(<Loading aria-label="Loading…" size="small" />)
         expect(getSize()).toEqual(originalSize)
     })
 
     it('renders with larger numeric sizes as the named size prop implies larger size', () => {
-        const { rerender } = render(<Loading label="Loading…" size="small" />)
+        const { rerender } = render(<Loading aria-label="Loading…" size="small" />)
         const smallSize = getSize()
 
-        rerender(<Loading label="Loading…" size="medium" />)
+        rerender(<Loading aria-label="Loading…" size="medium" />)
         const mediumSize = getSize()
 
-        rerender(<Loading label="Loading…" size="large" />)
+        rerender(<Loading aria-label="Loading…" size="large" />)
         const largeSize = getSize()
 
         expect(smallSize).toBeLessThan(mediumSize)
@@ -39,15 +53,25 @@ describe('Loading', () => {
     })
 
     it('renders in small size if an invalid size is given', () => {
-        const { rerender } = render(<Loading label="Loading…" size="small" />)
+        const { rerender } = render(<Loading aria-label="Loading…" size="small" />)
         const smallSize = getSize()
         rerender(
             <Loading
-                label="Loading…"
+                aria-label="Loading…"
                 // @ts-expect-error invalid value given on purpose
                 size="wrong"
             />,
         )
         expect(getSize()).toEqual(smallSize)
+    })
+
+    it('supports having its label set via aria-labelledby', () => {
+        render(
+            <>
+                <Loading aria-labelledby="label-id" />
+                <div id="label-id">Loading data…</div>
+            </>,
+        )
+        expect(screen.getByRole('progressbar', { name: 'Loading data…' })).toBeInTheDocument()
     })
 })
