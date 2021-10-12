309bd64789b8c4d6c3dc24740887fd138fdd814e
Craig Carlyle <craigcarlyle@me.com>
2021-10-12 16:35:43 -0700

    Removes camelCase props

--- src/new-components/checkbox-field/checkbox-field.tsx
+++ src/new-components/checkbox-field/checkbox-field.tsx
@@ -6,15 +6,25 @@ import { CheckboxIcon } from './checkbox-icon'
 
 import styles from './checkbox-field.module.css'
 
-type CheckboxFieldProps = Omit<JSX.IntrinsicElements['input'], 'type' | 'className'> & {
+type CheckboxFieldProps = Omit<
+    JSX.IntrinsicElements['input'],
+    | 'type'
+    | 'className'
+    | 'disabled'
+    | 'aria-controls'
+    | 'aria-describedby'
+    | 'aria-label'
+    | 'aria-labelledby'
+> & {
+    'aria-checked'?: never
     /** Identifies the set of checkboxes controlled by the mixed checkbox for assistive technologies. */
-    ariaControls?: string
+    'aria-controls'?: string
     /** Identifies the element (or elements) that describes the checkbox for assistive technologies. */
-    ariaDescribedby?: string
+    'aria-describedby'?: string
     /** Defines a string value that labels the current checkbox for assistive technologies. */
-    ariaLabel?: string
+    'aria-label'?: string
     /** Identifies the element (or elements) that labels the current checkbox for assistive technologies. */
-    ariaLabelledby?: string
+    'aria-labelledby'?: string
     /** Defines whether or not the checkbox is disabled. */
     disabled?: boolean
     /** The label for the checkbox element. */
