07b16b4fa65c9c93e1f59c0e29ea06b3b405815e
Craig Carlyle <craigcarlyle@me.com>
2021-10-18 16:00:07 -0700

    Adds a11y test, badge to Heading

--- src/new-components/heading/heading.stories.tsx
+++ src/new-components/heading/heading.stories.tsx
@@ -7,6 +7,9 @@ import { ResponsiveWidthRef, select, selectWithNone } from '../storybook-helper'
 export default {
     title: 'Design system/Heading',
     component: Heading,
+    parameters: {
+        badges: ['accessible'],
+    },
 }
 
 export function HeadingStory() {
