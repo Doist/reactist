1b9f12b507423169a7630b8ce3af97153a79ab0b
Ernesto García <ernesto@doist.com>
2022-07-12 07:56:25 -0400

    chore: Removes DeprecatedLoading, DeprecatedModal and Checkbox

--- src/components/deprecated-modal/index.ts
+++
@@ -1,5 +0,0 @@
-import { Box, Header, Body, Actions } from './deprecated-modal'
-import type { Modal } from './deprecated-modal'
-
-export default { Box, Header, Body, Actions }
-export type { Modal }
