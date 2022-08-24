c3c43a72292d9539aeb7464d29974e4301c72220
Frankie Yan <frankieyan@users.noreply.github.com>
2022-08-24 13:39:40 +0800

    deps: Update @geometricpanda/storybook-addon-badges to v0.2.2 (#687)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -15,27 +15,48 @@ export const parameters = {
     decorators,
 }
 
+const badgeFontStyles = {
+    fontSize: '12px',
+    lineHeight: '14px',
+}
+
 addParameters({
     badgesConfig: {
         accessible: {
-            contrast: 'rgba(5, 133, 39, 0.1)',
-            color: 'rgb(5, 133, 39)',
             title: '✔ Accessible (WCAG 2.0 AA)',
+            styles: {
+                backgroundColor: 'rgba(5, 133, 39, 0.1)',
+                borderColor: 'rgb(5, 133, 39)',
+                color: 'rgb(5, 133, 39)',
+                ...badgeFontStyles,
+            },
         },
         partiallyAccessible: {
-            contrast: 'rgba(235, 141, 19, 0.1)',
-            color: 'rgb(235, 141, 19)',
             title: '⚠ Partially Accessible',
+            styles: {
+                backgroundColor: 'rgba(235, 141, 19, 0.1)',
+                borderColor: 'rgb(235, 141, 19)',
+                color: 'rgb(235, 141, 19)',
+                ...badgeFontStyles,
+            },
         },
         notAccessible: {
-            contrast: 'rgba(209, 69, 59, 0.1)',
-            color: 'rgb(209, 69, 59)',
             title: '✖ Not accessible',
+            styles: {
+                backgroundColor: 'rgba(209, 69, 59, 0.1)',
+                borderColor: 'rgb(209, 69, 59)',
+                color: 'rgb(209, 69, 59)',
+                ...badgeFontStyles,
+            },
         },
         deprecated: {
-            contrast: 'rgba(209, 69, 59, 0.1)',
-            color: 'rgb(209, 69, 59)',
             title: '✖ Deprecated',
+            styles: {
+                backgroundColor: 'rgba(209, 69, 59, 0.1)',
+                borderColor: 'rgb(209, 69, 59)',
+                color: 'rgb(209, 69, 59)',
+                ...badgeFontStyles,
+            },
         },
     },
 })
