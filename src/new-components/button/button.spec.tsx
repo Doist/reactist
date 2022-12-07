f766609d5dc5638ab7f2319d431e2abed98067fa
Ernesto García <ernesto@doist.com>
2022-12-07 17:21:20 -0300

    Install and configure playwright

---
+++ src/new-components/button/button.spec.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { test, expect } from '@playwright/experimental-ct-react'
+import { Button } from './button'
+
+test.use({ viewport: { width: 500, height: 500 } })
+
+test('renders a button with the given label', async ({ mount }) => {
+    const component = await mount(<Button variant="primary">Click me</Button>)
+    expect(component.getByRole('button', { name: 'Click me' })).toBeDefined()
+})
