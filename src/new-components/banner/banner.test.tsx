dffbab1e3c2ea1500f3ddfd5069880411494b302
Francesca Guiducci <francesca@doist.com>
2023-01-30 14:31:27 +0100

    feat: New Banner component (#750)
    feat: New Banner component

---
+++ src/new-components/banner/banner.test.tsx
@@ -0,0 +1,127 @@
+import * as React from 'react'
+import { render, screen } from '@testing-library/react'
+import { axe } from 'jest-axe'
+import { Banner, BannerTone } from './banner'
+
+describe('Banner', () => {
+    it('renders as a <div /> element', () => {
+        render(<Banner tone="info" icon={'💚'} title={'Info'} data-testid="test-banner" />)
+        expect(screen.getByTestId('test-banner').tagName).toBe('DIV')
+    })
+
+    it('renders the title inside the badge element', () => {
+        render(<Banner tone="info" icon={'💚'} title={'New'} data-testid="test-banner" />)
+        expect(screen.getByTestId('test-banner')).toHaveTextContent('New')
+    })
+
+    test.each([
+        ['info' as BannerTone, 'title-info'],
+        ['promotion' as BannerTone, 'title-promotion'],
+    ])('renders a different CSS class according to the tone', (tone, expectedCSSClass) => {
+        render(
+            <>
+                <Banner tone={tone} icon={'💚'} title={'Info'} />
+            </>,
+        )
+        expect(screen.getByText('Info')).toHaveClass(expectedCSSClass)
+    })
+
+    it('passes through aria-related attributes', () => {
+        render(
+            <Banner
+                tone="info"
+                icon={'💚'}
+                title={'Info'}
+                data-testid="test-banner"
+                aria-hidden="true"
+            />,
+        )
+        expect(screen.getByTestId('test-banner')).toHaveAttribute('aria-hidden', 'true')
+    })
+
+    it('passes through data-* attributes', () => {
+        // Even though the use of data-testid already proves that the test passes, it may be important
+        // to assert that any data-* attribute is forwarded as well.
+        render(
+            <Banner
+                tone="info"
+                icon={'💚'}
+                title={'Info'}
+                data-testid="test-banner"
+                data-gtm-id="track-id"
+            />,
+        )
+        expect(screen.getByTestId('test-banner')).toHaveAttribute('data-gtm-id', 'track-id')
+    })
+
+    it('does not passes through other attributes such as className, or exceptionallySetClassName', () => {
+        render(
+            <Banner
+                tone="info"
+                icon={'💚'}
+                title={'Info'}
+                data-testid="test-banner"
+                // @ts-expect-error
+                className="test-one"
+                exceptionallySetClassName="test-two"
+            />,
+        )
+        expect(screen.getByTestId('test-banner')).not.toHaveClass('test-one')
+        expect(screen.getByTestId('test-banner')).not.toHaveClass('test-two')
+    })
+
+    it('renders more than one banner with no a11y violations', async () => {
+        const { container } = render(
+            <>
+                <Banner tone="info" icon={'💚'} title={'Info'} />
+                <Banner tone="promotion" icon={'🧡'} title={'Promotion'} />
+            </>,
+        )
+        const results = await axe(container)
+        expect(results).toHaveNoViolations()
+    })
+
+    it('honors rich text', () => {
+        render(
+            <Banner
+                tone="info"
+                icon={'💚'}
+                title={
+                    <>
+                        This is really <strong>important</strong>
+                    </>
+                }
+                data-testid="test-banner"
+            />,
+        )
+        expect(screen.getByTestId('test-banner').innerHTML).toContain(
+            'This is really <strong>important</strong>',
+        )
+    })
+
+    it('uses the title as the accessible name', () => {
+        render(<Banner tone="info" icon={'💚'} title={'Hello World'} />)
+        expect(screen.getByRole('status', { name: 'Hello World' })).toBeInTheDocument()
+    })
+
+    it('uses the description as the accessible description', () => {
+        render(
+            <Banner
+                tone="info"
+                icon={'💚'}
+                title={'Hello World'}
+                description={'Welcome to the world, Linus!'}
+            />,
+        )
+        expect(screen.getByRole('status', { name: 'Hello World' })).toHaveAccessibleDescription(
+            'Welcome to the world, Linus!',
+        )
+    })
+
+    it('does not have an accessible description if description is missing', () => {
+        render(<Banner tone="info" icon={'💚'} title={'Hello World'} />)
+        expect(
+            screen.getByRole('status', { name: 'Hello World' }),
+        ).not.toHaveAccessibleDescription()
+    })
+})
---
+++ src/new-components/banner/banner.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react'
+import { Box } from '../box'
+import { Columns, Column } from '../columns'
+import { useId } from '../common-helpers'
+
+import styles from './banner.module.css'
+
+export type BannerTone = 'info' | 'promotion'
+
+type BannerProps = {
+    id?: string
+    /**
+     * The tone of the Banner. Affects the background color and the outline.
+     */
+    tone: BannerTone
+    /**
+     * The icon that should be added inside the Banner.
+     */
+    icon: React.ReactChild
+    /**
+     * The title to be displayed at the top of the Banner.
+     */
+    title: React.ReactNode
+    /**
+     * An optional description to be displayed inside the Banner.
+     */
+    description?: React.ReactNode
+    /**
+     * An optional action to displayed inside the Banner.
+     */
+    action?: React.ReactChild
+}
+
+const Banner = React.forwardRef<HTMLDivElement, BannerProps>(function Banner(
+    { id, tone, icon, title, description, action, ...props }: BannerProps,
+    ref,
+) {
+    const titleId = useId()
+    const descriptionId = useId()
+    return (
+        <Box
+            {...props}
+            ref={ref}
+            id={id}
+            role="status"
+            aria-labelledby={titleId}
+            aria-describedby={descriptionId}
+            aria-live="polite"
+            tabIndex={0}
+            borderRadius="standard"
+            className={[styles.banner, styles[`banner-${tone}`]]}
+        >
+            <Columns space="medium" alignY="center">
+                <Column
+                    width="content"
+                    aria-hidden
+                    style={{
+                        /* Make sure the icon is centered vertically */
+                        lineHeight: 0,
+                    }}
+                >
+                    {icon}
+                </Column>
+                <Column>
+                    <Box paddingY="xsmall">
+                        {description ? (
+                            <Box id={titleId} className={[styles.title, styles[`title-${tone}`]]}>
+                                {title}
+                            </Box>
+                        ) : (
+                            <Box
+                                id={titleId}
+                                className={[
+                                    styles.title,
+                                    // If the banner does not have a description, we need to slightly tweak
+                                    // the styling of the title applying an extra css class
+                                    styles[`title-without-description`],
+                                    styles[`title-${tone}`],
+                                ]}
+                            >
+                                {title}
+                            </Box>
+                        )}
+                        {description ? (
+                            <Box
+                                id={descriptionId}
+                                className={[styles.description, styles[`description-${tone}`]]}
+                            >
+                                {description}
+                            </Box>
+                        ) : null}
+                    </Box>
+                </Column>
+                {action ? <Column width="content">{action}</Column> : null}
+            </Columns>
+        </Box>
+    )
+})
+
+export { Banner }
+export type { BannerProps }
