7199596b562ba28854aca542719d885b7985d079
Ernesto García <ernesto@doist.com>
2023-07-10 13:50:27 -0400

    styles: New menu styles (#792)

--- src/menu/index.ts
+++ src/menu/index.ts
@@ -1,10 +1,1 @@
-export {
-    Menu,
-    MenuButton,
-    ContextMenuTrigger,
-    MenuList,
-    MenuItem,
-    SubMenu,
-    MenuGroup,
-} from './menu'
-export type { MenuItemProps, MenuGroupProps } from './menu'
+export * from './menu'
