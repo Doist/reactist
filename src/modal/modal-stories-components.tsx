f0e972902d35ca05471f83c0c1880fff1d3843d1
Paweł Grimm <pawel@doist.com>
2026-05-19 12:56:46 -0500

    fix(storybook): render modal stories in preview root

--- src/modal/modal-stories-components.tsx
+++ src/modal/modal-stories-components.tsx
@@ -194,12 +194,15 @@ function Modal(props: WithOptionals<ModalProps, 'isOpen' | 'onDismiss' | 'width'
     const { isOpen, closeModal, width, height, hideOn } = React.useContext(ModalStoryContext)
 
     /**
-     * Needed to make sure the modals are mounted inside the story's #root element
+     * Needed to make sure the modals are mounted inside the story's root element
      * @see https://www.chromatic.com/docs/snapshots#why-isn%E2%80%99t-my-modal-or-dialog-captured
      */
     const getPortalElement = React.useCallback(() => {
         const div = document.createElement('div')
-        const portalRoot = document.getElementById('root') ?? document.body
+        const portalRoot =
+            document.getElementById('storybook-root') ??
+            document.getElementById('root') ??
+            document.body
         portalRoot.appendChild(div)
         return div
     }, [])
