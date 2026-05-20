159f26c44ebee0d26559ed6127fa49ac4f4ceb7b
Paweł Grimm <pawel@doist.com>
2026-05-20 10:17:20 -0500

    feat(storybook): restore a11y (and other) badges in toolbar (#1042)
    * test(storybook): add badge parameter resolver
    
    * feat(storybook): render badges in toolbar

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,6 +1,16 @@
-import { addons } from 'storybook/manager-api'
+import { addons, types } from 'storybook/manager-api'
+import { ADDON_ID, BadgesTool, TOOL_ID } from './badges'
 import theme from './theme'
 
 addons.setConfig({
     theme,
+})
+
+addons.register(ADDON_ID, () => {
+    addons.add(TOOL_ID, {
+        title: 'Badges',
+        type: types.TOOL,
+        match: ({ viewMode }) => viewMode === 'story' || viewMode === 'docs',
+        render: BadgesTool,
+    })
 })
