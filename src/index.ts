f6789d865e0a7fa5d98ad35879dbb8e78b0a6966
Rui Martins <rmartins90@users.noreply.github.com>
2026-07-23 11:57:54 +0200

    feat: add provisional Table component

--- src/index.ts
+++ src/index.ts
@@ -46,6 +46,7 @@ export * from './badge'
 export * from './expansion-panel'
 export * from './menu'
 export * from './modal'
+export * from './table'
 export * from './tabs'
 export * from './tooltip'
 
